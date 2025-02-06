17a3a3fd8478a26616686adf83d702e63a5f066a
Võ Hoàng Phúc (work) <phuc111239@gmail.com>
2025-02-06 22:51:03 +0700

    create map tiles

---
+++ assets/tiles/dugeon tiles ver-2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="dugeon tiles ver-2" tilewidth="18" tileheight="18" tilecount="320" columns="16">
+ <image source="../images/Environment/Dungeon Prison/Assets/Dungeon_Tileset.png" width="512" height="640"/>
+</tileset>
