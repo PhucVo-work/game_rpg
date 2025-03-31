834e7c1a39d8a3a7ba93986d4fb45900dd92e0b7
Võ Hoàng Phúc (work) <phuc111239@gmail.com>
2025-03-31 22:03:56 +0700

    add 3 map ver 1.0

--- lib/util/map_config.dart
+++ lib/util/map_config.dart
@@ -4,8 +4,6 @@ import 'package:game_rpg/decoration/potion_life.dart';
 import 'package:game_rpg/decoration/spikes.dart';
 import 'package:game_rpg/decoration/torch.dart';
 import 'package:game_rpg/enemies/boss1.dart';
-import 'package:game_rpg/enemies/boss2.dart';
-import 'package:game_rpg/enemies/boss3.dart';
 import 'package:game_rpg/enemies/goblin.dart';
 import 'package:game_rpg/enemies/imp.dart';
 import 'package:game_rpg/enemies/mini_boss.dart';
@@ -17,11 +15,74 @@ import '../enemies/MaskOrc.dart';
 import '../enemies/MiniOrc.dart';
 import '../enemies/Orc.dart';
 import '../enemies/OrcShaman.dart';
+import '../enemies/boss2.dart';
+import '../enemies/boss3.dart';
 import '../main.dart';
 
-// note boss2, orc, orc_shaman khi xuất hiện thì lỗi rất nhiều
 class MapConfig {
   static final List<Map<String, dynamic>> maps = [
+    {
+      'path': 'tiled/map5.json',
+      'bossBuilder': (Vector2 p) => Boss2(p),
+      'kidDialogues': ['talk_kid_5', 'talk_kid_6', 'talk_player_8'],
+      'wizardDialogues': [
+        'talk_wizard_4',
+        'talk_player_5',
+        'talk_wizard_5',
+        'talk_player_6',
+        'talk_wizard_6',
+      ],
+      'objects': [
+        'boss',
+        'kid',
+        'wizard',
+        'key',
+        'torch_empty',
+        'orc',
+        'orc_shaman',
+        'mini_boss',
+        'spikes',
+        'door',
+      ],
+    },
+    {
+      'path': 'tiled/map4.json',
+      'bossBuilder': (Vector2 p) => Boss1(p),
+      'kidDialogues': ['talk_kid_3', 'talk_kid_4', 'talk_player_4'],
+      'wizardDialogues': [
+        'talk_wizard_1',
+        'talk_player_1',
+        'talk_wizard_2',
+        'talk_player_2',
+        'talk_wizard_3',
+      ],
+      'objects': [
+        'boss',
+        'kid',
+        'wizard',
+        'goblin',
+        'mask_orc',
+        'mini_orc',
+        'potion'
+      ],
+    },
+    {
+      'path': 'tiled/map6.json',
+      'bossBuilder': (Vector2 p) => Boss3(p),
+      'kidDialogues': ['talk_kid_10', 'talk_kid_10', 'talk_player_11'],
+      'objects': [
+        'boss',
+        'kid',
+        'key',
+        'door',
+        'imp',
+        'torch_empty',
+        'torch',
+        'goblin',
+        'spikes',
+        'potion'
+      ],
+    },
     // {
     //   'path': 'tiled/map1.json',
     //   'bossBuilder': (Vector2 p) => Boss1(p),
@@ -49,8 +110,34 @@ class MapConfig {
     //   ],
     // },
     // {
-    //   'path': 'tiled/old_version/island.json',
+    //   'path': 'tiled/map2.json',
     //   'bossBuilder': (Vector2 p) => Boss1(p),
+    //   'kidDialogues': ['talk_kid_3', 'talk_kid_4', 'talk_player_4'],
+    //   'wizardDialogues': [
+    //     'talk_wizard_1',
+    //     'talk_player_1',
+    //     'talk_wizard_2',
+    //     'talk_player_2',
+    //     'talk_wizard_3',
+    //   ],
+    //   'objects': [
+    //     'boss',
+    //     'kid',
+    //     'door',
+    //     'wizard',
+    //     'key',
+    //     'spikes',
+    //     'orc',
+    //     'orc_shaman',
+    //     'potion',
+    //     'mini_boss',
+    //     'torch',
+    //     'empty_torch'
+    //   ],
+    // },
+    // {
+    //   'path': 'tiled/map.json',
+    //   'bossBuilder': (Vector2 p) => Boss2(p),
     //   'kidDialogues': ['talk_kid_5', 'talk_kid_6', 'talk_player_8'],
     //   'wizardDialogues': [
     //     'talk_wizard_4',
@@ -59,80 +146,41 @@ class MapConfig {
     //     'talk_player_6',
     //     'talk_wizard_6',
     //   ],
-    //   'objects': ['boss', 'kid', 'wizard', 'orc'],
+    //   'objects': [
+    //     'boss',
+    //     'kid',
+    //     'wizard',
+    //     'door',
+    //     'key',
+    //     'potion',
+    //     'goblin',
+    //     'spikes',
+    //     'orc',
+    //     'mini_boss',
+    //     'torch',
+    //     'torch_empty',
+    //     'imp'
+    //   ],
     // },
-    {
-      'path': 'tiled/map2.json',
-      'bossBuilder': (Vector2 p) => Boss1(p),
-      'kidDialogues': ['talk_kid_3', 'talk_kid_4', 'talk_player_4'],
-      'wizardDialogues': [
-        'talk_wizard_1',
-        'talk_player_1',
-        'talk_wizard_2',
-        'talk_player_2',
-        'talk_wizard_3',
-      ],
-      'objects': [
-        'boss',
-        'kid',
-        'door',
-        'wizard',
-        'key',
-        'spikes',
-        'orc',
-        'orc_shaman',
-        'potion',
-        'mini_boss',
-        'torch',
-        'empty_torch'
-      ],
-    },
-    {
-      'path': 'tiled/map.json',
-      'bossBuilder': (Vector2 p) => Boss2(p),
-      'kidDialogues': ['talk_kid_5', 'talk_kid_6', 'talk_player_8'],
-      'wizardDialogues': [
-        'talk_wizard_4',
-        'talk_player_5',
-        'talk_wizard_5',
-        'talk_player_6',
-        'talk_wizard_6',
-      ],
-      'objects': [
-        'boss',
-        'kid',
-        'wizard',
-        'door',
-        'key',
-        'potion',
-        'goblin',
-        'spikes',
-        'orc',
-        'mini_boss',
-        'torch',
-        'torch_empty',
-        'imp'
-      ],
-    },
-    {
-      'path': 'tiled/map3.json',
-      'bossBuilder': (Vector2 p) => Boss3(p),
-      'kidDialogues': ['talk_kid_10', 'talk_kid_11', 'talk_player_11'],
-      'objects': [
-        'boss',
-        'kid',
-        'potion',
-        'key',
-        'torch',
-        'door',
-        'spikes',
-        'potion',
-        'goblin',
-        'mini_boss',
-        'empty_torch',
-        'imp'
-      ],
-    },
+    // {
+    //   'path': 'tiled/map3.json',
+    //   'bossBuilder': (Vector2 p) => Boss3(p),
+    //   'kidDialogues': ['talk_kid_10', 'talk_kid_11', 'talk_player_11'],
+    //   'objects': [
+    //     'boss',
+    //     'kid',
+    //     'potion',
+    //     'key',
+    //     'torch',
+    //     'door',
+    //     'spikes',
+    //     'potion',
+    //     'goblin',
+    //     'mini_boss',
+    //     'empty_torch',
+    //     'imp'
+    //   ],
+    // },
   ];
 
   static WorldMapByTiled? getMap(int index) {
@@ -158,7 +206,7 @@ class MapConfig {
       'goblin': (p) => Goblin(p.position),
       'imp': (p) => Imp(p.position),
       'mini_boss': (p) => MiniBoss(p.position),
-      'empty_torch': (p) => Torch(p.position, empty: true),
+      'torch_empty': (p) => Torch(p.position, empty: true),
       'orc_shaman': (p) => OrcShaman(p.position),
       'mask_orc': (p) => MaskOrc(p.position),
       'mini_orc': (p) => MiniOrc(p.position),
